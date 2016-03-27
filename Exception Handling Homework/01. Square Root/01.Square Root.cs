710313ec86071fce875e139cd62dd8f87c4a50ae
Plamena Miteva <plamenamiteva@abv.bg>
2016-03-27 18:46:49 +0300

    Exception Handling Homework

---
+++ Exception Handling Homework/01. Square Root/01.Square Root.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+            double squareRoot = Math.Sqrt(number);
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Sqrt for negative numbers is undefined!");
+            }
+            Console.WriteLine(squareRoot);
+        }
+        catch (FormatException)
+        {
+            throw new Exception("Invalid number format");
+        }
+        catch (OverflowException)
+        {
+            throw new Exception("Invalid number");
+        }
+        finally
+        {
+
+            Console.WriteLine("Good bye");
+        }
+    }
+}
+
---
+++ Exception Handling Homework/02. Enter Numbers/02. Enter Numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class EnterNumbers
+    {
+        static void Main()
+        {     
+            ReadNumber(0, 100, 10);
+        }
+        public static void ReadNumber(int start, int end, int numberOfInputNumbers)
+        {
+            int counter = 0;
+            while(counter < numberOfInputNumbers)
+            {
+                Console.WriteLine("Please enter a number value in the range [{0}....{1}]:", start, end);
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number < start)
+                    {
+                        Console.WriteLine("Invalid number. The input number should be greater than {0}.", start);
+                    }
+                    else if (number > end)
+                    {
+                        Console.WriteLine("Invalid number. The input number should be less than {0}.", end);
+                    }
+                        start = number;
+                        counter++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number format");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+            }
+
+        }
+    }
+
